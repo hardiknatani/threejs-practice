0a5a9cd2b119755e62a7049c80bbac399e3b7298
hardiknatani <hardiknatani202@gmail.com>
2023-02-28 12:11:36 +0530

    base scene

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as Three from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'threejs-practice';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild ('canvas') canvasRef!:ElementRef<HTMLCanvasElement>
+  scene = new Three.Scene();
+  camera = new Three.PerspectiveCamera(75,window.innerWidth/innerHeight);
+  renderer!: Three.WebGLRenderer
+
+
+
+
+  createScene(){
+    this.camera.position.set(50,50,50);
+    this.renderer.setClearColor("#233143")
+    this.renderer.setSize(window.innerWidth,window.innerHeight);
+  }
+
+  startRendering(){
+    let that = this;
+  (function render(){
+    requestAnimationFrame(render)
+    that.renderer.render(that.scene,that.camera);
+  })()
+
+  }
+
+  ngAfterViewInit(): void {
+
+    window.addEventListener('resize',()=>{
+      this.renderer.setSize(window.innerWidth,window.innerHeight);
+      this.camera.aspect = window.innerWidth/window.innerHeight;
+      this.camera.updateProjectionMatrix()
+    })
+
+    this.renderer = new Three.WebGLRenderer({
+      canvas : this.canvasRef.nativeElement,
+      antialias:true
+    });
+
+    this.createScene();
+    this.startRendering()
+
+    
+
+  }
+
 }
